6241a0164eb4740b79a662434ed0dcfe762b43ae
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-07-12 21:36:51 -0500

    Nuget dependencies added and Application Layer configurated

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+
+        /*
+         * Use ctrl + r + g to remove the unnecessary usings 
+         * **/
+
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            // Registra automaticamente los mapeos que se hagan en esta biblioteca de clase
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
