ae17054cb08f767a33026155b0934672cd628fe9
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-07-13 21:16:05 -0500

    Removing the commnets which refered to a visual studio shortcut

--- Application/ServiceExtensions.cs
+++ Application/ServiceExtensions.cs
@@ -7,11 +7,6 @@ namespace Application
 {
     public static class ServiceExtensions
     {
-
-        /*
-         * Use ctrl + r + g to remove the unnecessary usings 
-         * **/
-
         public static void AddApplicationLayer(this IServiceCollection services)
         {
             // Registra automaticamente los mapeos que se hagan en esta biblioteca de clase
