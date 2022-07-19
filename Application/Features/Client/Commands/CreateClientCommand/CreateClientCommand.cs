7c5588eb990a4836c6f6d27f94c949f54a0cc3f0
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-07-19 10:19:45 -0500

    A little change in CreateClientCommand file saved

--- Application/Features/Client/Commands/CreateClientCommand/CreateClientCommand.cs
+++ Application/Features/Client/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Wrappers;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Client.Commands.CreateClientCommand
 {
-    public class CreateClientCommand
+    public class CreateClientCommand : IRequest<Response<int>>
     {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
     }
 }
