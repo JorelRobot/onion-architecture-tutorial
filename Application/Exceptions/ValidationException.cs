a7457cd287f8e634b0baf41e3b57e65b050561bf
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-07-18 19:53:30 -0500

    ADD: Validator behaviour & exception
    This classes were created to manage custom validations using exception
    all of this through a pipeline behavoiur

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> Errors { get; }
+
+        public ValidationException() : base("It was produced one or more validation errors")
+        {
+            Errors = new List<string>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
