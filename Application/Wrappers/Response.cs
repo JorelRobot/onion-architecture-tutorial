0aa44886b84831ed4374d20941b8c663bc801b98
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-07-19 10:08:18 -0500

    ADD: Chapter 08 - First Domain Entity Created

---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wrappers
+{
+    public class Response<T>
+    {
+        public bool Succeeded { get; set; }
+        public string Messagge { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+
+        public Response()
+        {
+
+        }
+
+        public Response(T data, string message = null)
+        {
+            Succeeded = true;
+            Messagge = message;
+            Data = data;
+        }
+
+        public Response(string messagge)
+        {
+            Succeeded = false;
+            Messagge = messagge;
+        }
+    }
+}
---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Client : AuditableBaseEntity
+    {
+        private int _edad;
+
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Edad
+        {
+            get
+            {
+                if (this._edad <= 0)
+                {
+                    this._edad = new DateTime(DateTime.Now.Subtract(this.Birthday).Ticks).Year - 1;
+                }
+
+                return this._edad;
+            }
+        }
+    }
+}
