38fef76024de68ad1f18e641ffbf0233f20ba15f
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-07-23 15:15:15 -0500

    ADD: Chapter 08 - Repository Pattern & Persistence (Finished)

--- Application/Features/Client/Commands/CreateClientCommand/CreateClientCommand.cs
+++ Application/Features/Client/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -11,9 +11,18 @@ namespace Application.Features.Client.Commands.CreateClientCommand
     public class CreateClientCommand : IRequest<Response<int>>
     {
         public string Name { get; set; }
+        public string LastName { get; set; }
         public DateTime Birthday { get; set; }
         public string Telephone { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
+    }
+
+    public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Response<int>>
+    {
+        public Task<Response<int>> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
